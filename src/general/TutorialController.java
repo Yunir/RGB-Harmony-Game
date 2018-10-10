ea5550837a691c9ed46e3b36000124724ca5cec0
Yunir <yunir.vw@gmail.com>
2018-10-10 17:43:42 +0300

    Add keyListener to Tutorial fxml to close window by the Enter or Escape Key

--- src/general/TutorialController.java
+++ src/general/TutorialController.java
@@ -2,6 +2,8 @@ package general;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
@@ -15,7 +17,16 @@ public class TutorialController {
     }
 
     @FXML
+    public void checkKeyPressed(KeyEvent keyEvent) {
+        if (keyEvent.getCode() == KeyCode.ENTER || keyEvent.getCode() == KeyCode.ESCAPE) closeStage();
+    }
+
+    @FXML
     public void closeTutorial(MouseEvent mouseEvent) {
+        closeStage();
+    }
+
+    private void closeStage() {
         Stage currentStage = (Stage)(vGotItButton.getScene().getWindow());
         currentStage.close();
     }
